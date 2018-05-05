11214640769839435df65d16ca2d36eb6c0a35b2
Hakaba Hitoyo <example@example.com>
2018-05-05 18:46:18 +0900

    who to follow panel with third party provider

--- widgets/side_who_to_follow.php
+++ widgets/side_who_to_follow.php
@@ -39,3 +39,71 @@
     </li>
   </ul>
 </div>
+
+<script type="application/javascript">
+function showWhoToFollow (reply) {
+  var users = reply.ids
+  var cn
+  var index = 0
+  var random = Math.floor(Math.random() * 10)
+  for (cn = random; cn < users.length; cn = cn + 10) {
+    var user
+    user = users[cn]
+    var img
+    if (user.icon) {
+      img = user.icon
+    } else {
+      img = '/assets/images/halcyon_logo.png'
+    }
+    var name = user.to_id
+    if (index === 0) {
+      document.getElementById('who-to-follow-avatar-0').setAttribute('src',img)
+      document.getElementById('who-to-follow-link-0').innerHTML = encodeURI(name)
+      document.getElementById('who-to-follow-link-0').setAttribute('href','/@' + encodeURI(name))
+    } else if (index === 1) {
+      document.getElementById('who-to-follow-avatar-1').setAttribute('src',img)
+      document.getElementById('who-to-follow-link-1').innerHTML = encodeURI(name)
+      document.getElementById('who-to-follow-link-1').setAttribute('href','/@' + encodeURI(name))
+    } else if (index === 2) {
+      document.getElementById('who-to-follow-avatar-2').setAttribute('src',img)
+      document.getElementById('who-to-follow-link-2').innerHTML = encodeURI(name)
+      document.getElementById('who-to-follow-link-2').setAttribute('href','/@' + encodeURI(name))
+    }
+    index = index + 1
+    if (index > 2) {
+      break
+    }
+  }
+}
+
+function getWhoToFollow () {
+  var user = localStorage.getItem("current_id")
+  var appSettings = parse_ini_file('../config.ini',true)
+  var whoToFollowProvider = appSettings["App"]["who_to_follow_provider"]
+  if (user && whoToFollowProvider) {
+    var host = window.location.hostname
+    var url
+    url = whoToFollowProvider.replace(/{{host}}/g, encodeURIComponent(host))
+    url = url.replace(/{{user}}/g, encodeURIComponent(user))
+    window.fetch(url, {mode: 'cors'}).then(function (response) {
+      if (response.ok) {
+        return response.json()
+      } else {
+        document.getElementById('who-to-follow-link-0').innerHTML = ''
+        document.getElementById('who-to-follow-link-0').removeAttribute('href')
+        document.getElementById('who-to-follow-link-1').innerHTML = ''
+        document.getElementById('who-to-follow-link-1').removeAttribute('href')
+        document.getElementById('who-to-follow-link-2').innerHTML = ''
+        document.getElementById('who-to-follow-link-2').removeAttribute('href')
+      }
+    }).then(function (reply) {
+      showWhoToFollow(reply)
+    })
+  }
+}
+
+window.addEventListener ('load', function () {
+  getWhoToFollow()
+}, true)
+</script>
+
