e3018cb45cec961d64a92a753b55ec5a43faf867
Hakaba Hitoyo <example@example.com>
2018-05-05 18:04:35 +0900

    who to follow panel ui

--- widgets/side_who_to_follow.php
+++ widgets/side_who_to_follow.php
@@ -1,56 +1,41 @@
 <div class="side_widget what_to_follow">
-<h2>Who to follow</h2>
-<ul class="account_list">
-<li class="account_box what_to_follow_0">
-<div class="icon_box">
-<img src="">
-</div>
-<div class="label_box">
-<a href="">
-<h3>
-<span class="dn emoji_poss"></span>
-<span class="un"></span>
-</h3>
-</a>
-<button class="follow_button" mid="" data="">
-<i class="fa fa-fw fa-user-plus"></i>
-<span>Follow</span>
-</button>
-</div>
-</li>
-<li class="account_box what_to_follow_1">
-<div class="icon_box">
-<img src="">
-</div>
-<div class="label_box">
-<a href="">
-<h3>
-<span class="dn emoji_poss"></span>
-<span class="un"></span>
-</h3>
-</a>
-<button class="follow_button" mid="" data="">
-<i class="fa fa-fw fa-user-plus"></i>
-<span>Follow</span>
-</button>
-</div>
-</li>
-<li class="account_box what_to_follow_2">
-<div class="icon_box">
-<img src="">
-</div>
-<div class="label_box">
-<a href="">
-<h3>
-<span class="dn emoji_poss"></span>
-<span class="un"></span>
-</h3>
-</a>
-<button class="follow_button" mid="" data="">
-<i class="fa fa-fw fa-user-plus"></i>
-<span>Follow</span>
-</button>
-</div>
-</li>
-</ul>
+  <h2>Who to follow</h2>
+  <ul class="account_list">
+    <li class="account_box what_to_follow_0">
+      <div class="icon_box">
+	<img id="who-to-follow-avatar-0" src="/assets/images/halcyon_logo.png">
+      </div>
+      <div class="label_box">
+	<a id="who-to-follow-link-0">
+	  <h3>
+	    <span class="un" id="who-to-follow-id-0">Loading...</span>
+	  </h3>
+	</a>
+      </div>
+    </li>
+    <li class="account_box what_to_follow_1">
+      <div class="icon_box">
+	<img id="who-to-follow-avatar-1" src="/assets/images/halcyon_logo.png">
+      </div>
+      <div class="label_box">
+	<a id="who-to-follow-link-1">
+	  <h3>
+	    <span class="un" id="who-to-follow-id-1">Loading...</span>
+	  </h3>
+	</a>
+      </div>
+    </li>
+    <li class="account_box what_to_follow_2">
+      <div class="icon_box">
+	<img id="who-to-follow-avatar-2" src="/assets/images/halcyon_logo.png">
+      </div>
+      <div class="label_box">
+	<a id="who-to-follow-link-2">
+	  <h3>
+	    <span class="un" id="who-to-follow-id-2">Loading...</span>
+	  </h3>
+	</a>
+      </div>
+    </li>
+  </ul>
 </div>
